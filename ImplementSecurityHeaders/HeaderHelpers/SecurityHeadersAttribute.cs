c44c9a8fc3b4ed444b62417db90ece88d0bd3863
SandeepKandagatla <59524137+SandeepKandagatla@users.noreply.github.com>
2020-03-15 20:24:39 +0530

    Add files via upload

---
+++ ImplementSecurityHeaders/HeaderHelpers/SecurityHeadersAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImplementSecurityHeaders.HeaderHelpers
+{
+    public class SecurityHeadersAttribute:ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            var result = context.Result;
+            if (result is ViewResult)
+            {
+                context.HttpContext.Response.Headers.Add("HeaderType", "HeaderValue");
+            }
+        }
+    }
+}
---
+++ ImplementSecurityHeaders/HeaderHelpers/SecurityHeadersMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ImplementSecurityHeaders.HeaderHelpers
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class SecurityHeadersMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public SecurityHeadersMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Response.Headers.Add("HeaderType", "HeaderValue");
+            httpContext.Response.Headers.Add("Content-Security-Policy","style-src 'none'");//no css
+            httpContext.Response.Headers.Add("Content-Security-Policy", "style-src 'none' 'unsafe inline'");//allow inline css
+            httpContext.Response.Headers.Add("Content-Security-Policy", "style-src 'self'"+
+                "https://stackpath.bootstrapcdn.com" +//bootstrap is usable
+                "frame-ancestors ''none");//no iframes
+
+            //httpContext.Response.Headers.Add("X-Frame-Options","ALLOW-FROM 'https://example.com'");
+            //httpContext.Response.Headers.Add("X-Frame-Options", "DENY");
+            httpContext.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+            httpContext.Response.Headers.Add("Feature-Policy", "camera 'none'");
+            httpContext.Response.Headers.Add("X-Content-Type-Options", "nosniff");//turnoff the MIME type sniffing to prevent XSS
+            httpContext.Response.Headers.Add("Content-Security-Policy", "require-sri-for 'script' 'style'");//use integrity has for cdn links in css and script
+            httpContext.Response.Headers.Add("Referrer-Policy", "no-referrer");
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class SeurityHeadersMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSeurityHeadersMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<SecurityHeadersMiddleware>();
+        }
+    }
+}
